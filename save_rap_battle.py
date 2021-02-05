11d3777693f851ba5c9fdc30cc0d4f91fb49e8aa
chorro73 <chorro73@gmail.com>
2021-02-04 16:20:10 -0800

    implement threading first pass (bugged)

--- save_rap_battle.py
+++ save_rap_battle.py
@@ -1,5 +1,6 @@
 import sys, requests
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 
 BASE_URL = r'https://fishery.jackboxgames.com/artifact/gallery/RapBattleGame/'
 BASE_IMG_URL = r'https://s3.amazonaws.com/jbg-blobcast-artifacts/'
@@ -18,16 +19,21 @@ response = requests.get(BASE_URL + battle_id)
 data = response.json()
 rounds = data['gameData']
 
-for r in rounds:
-    round_name = r['title']
-    for c in r['children']:
-        player_name = c['title']
-        file_path = destination_folder / (round_name + '_' + player_name + '.gif')
+def download_image(uri, dest):
+    print(f'Downloading {uri} to {dest}')
+    content = requests.get(uri).content
+    file_path.write_bytes(content)
+    print(f'Downloaded {dest}')
 
-        image_uri = BASE_IMG_URL + c['twitterOptions']['imageGifUri']
-        print(f'Downloading {round_name} image for {player_name}: {image_uri}')
-        content = requests.get(image_uri).content
-        file_path.write_bytes(content)
+futures = []
+with ThreadPoolExecutor() as executor:
+    for r in rounds:
+        round_name = r['title']
+        for c in r['children']:
+            player_name = c['title']
+            file_path = destination_folder / (round_name + '_' + player_name + '.gif')
+            image_uri = BASE_IMG_URL + c['twitterOptions']['imageGifUri']
+            futures.append(executor.submit(download_image, image_uri, file_path))
         
-print("Done!")
-
+for f in futures:
+    print(f.exception())

f8c9c2f41ee6a512dcef7d6d7ff05e517a965cb7
chorro73 <chorro73@gmail.com>
2021-02-04 22:09:07 -0800

    format and fix download_image

--- save_rap_battle.py
+++ save_rap_battle.py
@@ -1,39 +1,40 @@
-import sys, requests
+import sys
+import requests
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
-BASE_URL = r'https://fishery.jackboxgames.com/artifact/gallery/RapBattleGame/'
-BASE_IMG_URL = r'https://s3.amazonaws.com/jbg-blobcast-artifacts/'
+BASE_URL = r"https://fishery.jackboxgames.com/artifact/gallery/RapBattleGame/"
+BASE_IMG_URL = r"https://s3.amazonaws.com/jbg-blobcast-artifacts/"
 
 _, battle_id, destination_folder = sys.argv
 
 destination_folder = Path(destination_folder)
 if destination_folder.exists():
-    i = input(f'{destination_folder} already exists, overwrite? (Y/N)')
-    if i.lower() != 'y':
+    i = input(f"{destination_folder} already exists, overwrite? (Y/N)")
+    if i.lower() != "y":
         exit()
 
 destination_folder.mkdir(parents=True, exist_ok=True)
 
 response = requests.get(BASE_URL + battle_id)
 data = response.json()
-rounds = data['gameData']
+rounds = data["gameData"]
 
-def download_image(uri, dest):
-    print(f'Downloading {uri} to {dest}')
+
+def download_image(uri, path):
+    print(f"Downloading {uri} to {path}")
     content = requests.get(uri).content
-    file_path.write_bytes(content)
-    print(f'Downloaded {dest}')
+    path.write_bytes(content)
+    print(f"Downloaded {path}")
+    return True
 
-futures = []
-with ThreadPoolExecutor() as executor:
-    for r in rounds:
-        round_name = r['title']
-        for c in r['children']:
-            player_name = c['title']
-            file_path = destination_folder / (round_name + '_' + player_name + '.gif')
-            image_uri = BASE_IMG_URL + c['twitterOptions']['imageGifUri']
-            futures.append(executor.submit(download_image, image_uri, file_path))
-        
-for f in futures:
-    print(f.exception())
+
+executor = ThreadPoolExecutor()
+
+for r in rounds:
+    round_name = r["title"]
+    for c in r["children"]:
+        player_name = c["title"]
+        file_path = destination_folder / (round_name + "--" + player_name + ".gif")
+        image_uri = BASE_IMG_URL + c["twitterOptions"]["imageGifUri"]
+        executor.submit(download_image, image_uri, file_path)
